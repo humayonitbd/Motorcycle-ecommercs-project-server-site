5bd9884801685cd56fc9e0448749d6f8fcbdfacc
humayonitbd <humayonforid44@gmail.com>
2022-11-24 20:57:17 +0600

    booking post

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -23,12 +23,42 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try {
         const bikeCategorysCollection = client.db('bike-resel-project').collection('bike-categorys');
-
+        const bikeAllCategoryCollection = client.db('bike-resel-project').collection('bike-all-categorys');
+        const bookingProductsCollection = client.db('bike-resel-project').collection('bookingProducts');
+        //category get api
         app.get('/categorys', async(req, res)=>{
             const query = {};
             const categorys = await bikeCategorysCollection.find(query).toArray();
             res.send(categorys)
         })
+
+        //single category api
+        app.get('/category/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await bikeCategorysCollection.findOne(query);
+            res.send(result)
+
+        })
+
+        //all category get
+        app.get('/allCategory', async(req, res)=>{
+            const category = req.query.category;
+            console.log(category)
+            const bandAllCategory = {category: category}
+            const bandCategory = await bikeAllCategoryCollection.find(bandAllCategory).toArray();
+            res.send(bandCategory)
+        })
+
+
+        //booking product data post 
+        app.post('/bookedProduct', async(req, res)=>{
+            const body = req.body;
+            const bookingproduct = await bookingProductsCollection.insertOne(body);
+            res.send(bookingproduct)
+
+        })
+
         
     } catch (error) {
         console.log(error.message)
