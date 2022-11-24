d4d2f1725e611e64d5ae748bda6518e63858152d
humayonitbd <humayonforid44@gmail.com>
2022-11-24 11:17:52 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 
 //middle ware
@@ -11,6 +13,23 @@ app.use(express.json())
 app.get('/', async(req, res)=>{
     res.send('server is running...')
 })
+
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.epqkzkd.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    
+
+}
+run().catch(console.log)
+
+
+
+
+
 
 
 app.listen(port,()=>{
