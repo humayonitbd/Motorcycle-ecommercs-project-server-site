52becec438f2b8bc0c431d9f0f3875841dbd8787
humayonitbd <humayonforid44@gmail.com>
2022-11-24 09:34:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middle ware
+app.use(cors());
+app.use(express.json())
+
+app.get('/', async(req, res)=>{
+    res.send('server is running...')
+})
+
+
+app.listen(port,()=>{
+    console.log('your server running port is ', port)
+})
